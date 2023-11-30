a3c540267b6c007a75b2103b397bb9316a4de710
Ben <bennette.benoy@outlook.com>
2023-11-30 18:17:49 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <nav>
+        <h2>BENB_DB</h2>
+        <a>PROJECTS</a>
+        <a>GITHUB</a>
+        <a>CONTACT</a>
+      </nav>
+      <div id="intro-page">
+        <h1>Hi I'm Ben.</h1>
+        <p id="description-1">
+          As a Software Engineering student with an interest in Web, Game, and
+          AI development, I'm always looking to create new digital experiences.
+        </p>
+        <p id="description-2">See what I've done so far:</p>
+      </div>
+      <div id="projects">Hello</div>
+    </>
+  );
+}
+
+export default App;
